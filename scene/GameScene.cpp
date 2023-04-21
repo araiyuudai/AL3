9f42cf13c11b0e52ccbd1d5a4879804822fccf31
furann0811@outlook.com <furann0811@outlook.com>
2023-04-21 12:22:24 +0900

    ステージとプレイヤーと弾の発射

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,20 +1,75 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include "MathUtilityForText.h"
 
+
+//コンストラクタ
 GameScene::GameScene() {}
+//デストラクタ
+GameScene::~GameScene() {
 
-GameScene::~GameScene() {}
+delete spriteBG_;
+	delete modelStage_;
 
+delete modelPlayer_;
+
+
+delete modelBeam_;
+
+}
+//初期化
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+
+textureHandleBG_ = TextureManager::Load("bg.jpg");
+	spriteBG_ = Sprite::Create(textureHandleBG_, {0, 0});
+
+	
+	viewProjection_.translation_.y = 1;
+	viewProjection_.translation_.z = -6;
+	viewProjection_.Initialize();
+
+	textureHandleStage_ = TextureManager::Load("stage.jpg");
+	modelStage_ = Model::Create();
+	worldTransformStage_.Initialize();
+
+	worldTransformStage_.translation_ = {0, -1.5f, 0};
+	worldTransformStage_.scale_ = {4.5f, 1, 40};
+
+	worldTransformStage_.matWorld_ = MakeAffineMatrix(
+	    worldTransformStage_.scale_, worldTransformStage_.rotation_,
+	    worldTransformStage_.translation_);
+
+	worldTransformStage_.TransferMatrix();
+
+
+textureHandlePlayer_ = TextureManager::Load("player.png");
+modelPlayer_ = Model::Create();
+
+worldTransformPlayer_.scale_ = {0.5f, 0.5f, 0.5f};
+worldTransformPlayer_.Initialize();
+
+
+textureHandleBeam_ = TextureManager::Load("beam.png");
+modelBeam_ = Model::Create();
+
+worldTransformBeam_.scale_ = {0.3f, 0.3f, 0.3f};
+worldTransformBeam_.Initialize();
+
+}
+//更新
+void GameScene::Update() { 
+	
+	PlayerUpdate(); 
+	BeamUpdate();
+
 }
 
-void GameScene::Update() {}
-
+//描画
 void GameScene::Draw() {
 
 	// コマンドリストの取得
@@ -27,6 +82,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
+	spriteBG_->Draw();
+
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
@@ -41,7 +98,16 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	modelStage_->Draw(worldTransformStage_, viewProjection_, textureHandleStage_);
 
+
+	modelPlayer_->Draw(worldTransformPlayer_, viewProjection_, textureHandlePlayer_);
+
+	
+	if (beamFlag_ == 1) {
+		modelBeam_->Draw(worldTransformBeam_, viewProjection_, textureHandleBeam_);
+	}	
+	
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
@@ -58,4 +124,66 @@ void GameScene::Draw() {
 	Sprite::PostDraw();
 
 #pragma endregion
+}
+
+//プレイヤー更新
+void GameScene::PlayerUpdate() {
+	worldTransformPlayer_.matWorld_ = MakeAffineMatrix(
+	    worldTransformPlayer_.scale_, worldTransformPlayer_.rotation_,
+	    worldTransformPlayer_.translation_);
+
+	worldTransformPlayer_.TransferMatrix();
+
+	//右へ移動
+	if (input_->PushKey(DIK_RIGHT)) {
+		worldTransformPlayer_.translation_.x += 0.1f;
+	}
+	//左に移動
+	if (input_->PushKey(DIK_LEFT)) {
+		worldTransformPlayer_.translation_.x -= 0.1f;
+	}
+	if (worldTransformPlayer_.translation_.x > 4) {
+		worldTransformPlayer_.translation_.x = 4;
+	}
+	if (worldTransformPlayer_.translation_.x < -4) {
+		worldTransformPlayer_.translation_.x = -4;
+	}
+
+}
+//ビーム更新
+void GameScene::BeamUpdate() {
+	worldTransformBeam_.matWorld_ = MakeAffineMatrix(
+	    worldTransformBeam_.scale_, worldTransformBeam_.rotation_,
+	    worldTransformBeam_.translation_);
+
+	worldTransformBeam_.TransferMatrix();
+
+	BeamMove();
+	BeamBorn();
+
+}
+
+void GameScene::BeamMove() {
+	
+	if (beamFlag_ == 1) {
+		worldTransformBeam_.translation_.z += 1.0f;
+	}
+	if (beamFlag_ == 1) {
+		if (worldTransformBeam_.translation_.z > 40) {
+			beamFlag_ = 0;
+		
+			worldTransformBeam_.translation_.z = worldTransformPlayer_.translation_.z;
+		}
+	}
+	worldTransformBeam_.rotation_.x += 0.1f;
+}
+
+void GameScene::BeamBorn() {
+
+	if (beamFlag_ == 0) {
+		if (input_->PushKey(DIK_SPACE)) {
+			worldTransformBeam_.translation_.x = worldTransformPlayer_.translation_.x;
+			beamFlag_ = 1;
+		}
+	}
 }
